047a6e1f62f6638df6b99137b5066a6d6f8c7307
Vivekpatel <vp20032020@gmail.com>
2025-02-28 14:52:31 +0530

    [Vivek] Added UC7

--- Employee-Payroll-App/src/main/java/com/Employee/Payroll/App/Employee/Payroll/App/dto/EmployeeDTO.java
+++ Employee-Payroll-App/src/main/java/com/Employee/Payroll/App/Employee/Payroll/App/dto/EmployeeDTO.java
@@ -4,17 +4,38 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Data
-
+@Slf4j
 // Generates getters, setters, toString, equals, and hashCode
 @NoArgsConstructor // Generates a no-argument constructor
 @AllArgsConstructor // Generates a constructor with all fields
 public class EmployeeDTO {
     private String name;
     private double salary;
-
+    public EmployeeDTO(String name, double salary)
+    {
+        this.name=name;
+        this.salary=salary;
     }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
 
 
 
